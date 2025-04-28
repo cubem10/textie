d4433740d4bf8a43ba714c26ae43ea6117107bf7
cubem10 <cubem10@kakao.com>
2025-04-28 23:47:39 +0900

    LoginView: use KeyStore to save credentials
    Signed-off-by: cubem10 <cubem10@kakao.com>

--- Textie/ContentView.swift
+++ Textie/ContentView.swift
@@ -8,6 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    func login() throws {
+        let query: [String: Any] = [kSecClass as String: kSecClassGenericPassword,
+                                            kSecAttrService as String: "Textie",
+                                            kSecMatchLimit as String: kSecMatchLimitOne,
+                                            kSecReturnAttributes as String: true,
+                                            kSecReturnData as String: true]
+        
+        var item: CFTypeRef?
+        let status = SecItemCopyMatching(query as CFDictionary, &item)
+        guard status != errSecItemNotFound else {
+            throw KeyChainError.noPassword
+        }
+        guard status == errSecSuccess else {
+            throw KeyChainError.unhandledError(status: status)
+        }
+    }
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
--- Textie/LoginView.swift
+++ Textie/LoginView.swift
@@ -12,12 +12,53 @@ struct LoginView: View {
     @State private var credential = Credential(id: "", password: "")
     @State private var invaildCredentials = false
     @State private var showRegisterView = false
+    @State private var loginFailed: Bool = false
         
-    func login() {
+    func login() throws {
         // TODO: implement login API call
         if credential.id == "test" && credential.password == "testtest" {
             invaildCredentials = true
+            return
         }
+        
+        let account = credential.id
+        let password = credential.password.data(using: String.Encoding.utf8)!
+        let query: [String: Any] = [kSecClass as String: kSecClassGenericPassword,
+                                    kSecAttrService as String: "Textie",
+                                    kSecAttrAccount as String: account,
+                                    kSecValueData as String: password]
+        
+        let deleteStatus = SecItemDelete(query as CFDictionary)
+        guard deleteStatus == errSecSuccess || deleteStatus == errSecItemNotFound else {
+            throw KeyChainError.unhandledError(status: deleteStatus)
+        }
+        
+        let status = SecItemAdd(query as CFDictionary, nil)
+        guard status == errSecSuccess else {
+            throw KeyChainError.unhandledError(status: status)
+        }
+    }
+    
+    func loadSavedCredential() {
+        let query: [String: Any] = [kSecClass as String: kSecClassGenericPassword,
+                                    kSecAttrService as String: "Textie",
+                                    kSecMatchLimit as String: kSecMatchLimitOne,
+                                    kSecReturnAttributes as String: true,
+                                    kSecReturnData as String: true]
+        
+        var item: CFTypeRef?
+        let status = SecItemCopyMatching(query as CFDictionary, &item)
+        if status == errSecItemNotFound || status != errSecSuccess {
+            return
+        }
+        
+        guard let existingItem = item as? [String: Any], let passwordData = existingItem[kSecValueData as String] as? Data, let password = String(data: passwordData, encoding: String.Encoding.utf8), let account = existingItem[kSecAttrAccount as String] as? String
+        else {
+            return
+        }
+        
+        credential.id = account
+        credential.password = password
     }
     
     var body: some View {
@@ -29,7 +70,13 @@ struct LoginView: View {
             Divider()
             PasswordFieldView(password: $credential.password, placeholder: "Password").padding()
             HStack {
-                Button(action: login) {
+                Button(action: {
+                    do {
+                        try login()
+                    } catch {
+                        loginFailed = true
+                    }
+                }) {
                     Text("Sign In")
                 }.disabled(credential.id == "" || credential.password.count < 8)
                     .alert(isPresented: $invaildCredentials) {
@@ -40,11 +87,21 @@ struct LoginView: View {
                 }.sheet(isPresented: $showRegisterView) {
                     RegisterView(showRegisterView: $showRegisterView, credential: $credential)
                         .onChange(of: showRegisterView) {
-                            login()
+                            do {
+                                try login()
+                            } catch {
+                                loginFailed = true
+                            }
                         }
+                }
+                .alert(isPresented: $loginFailed) {
+                    Alert(title: Text("Login Failed"), message: Text("Please try again later."))
                 }
             }
         }.padding()
+            .onAppear {
+                loadSavedCredential()
+            }
     }
 }
 
