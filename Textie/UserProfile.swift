40f48ca0544e370e020024f84a0a31ec7b3debd0
cubem10 <cubem10@kakao.com>
2025-04-30 18:20:13 +0900

    UserProfile: declare UserProfile and ProfileManager for managing user profile
    Signed-off-by: cubem10 <cubem10@kakao.com>

---
+++ Textie/UserProfile.swift
@@ -0,0 +1,26 @@
+//
+//  UserProfile.swift
+//  Textie
+//
+//  Created by 하정우 on 4/30/25.
+//
+
+import Foundation
+
+struct UserProfile {
+    var name: String
+    var userId: String
+
+    var bio: String?
+    var profileImageURL: URL?
+    var birthDate: Date?
+}
+
+@Observable
+class ProfileManager {
+    var profile: UserProfile?
+    
+    func loadProfile(_ userSession: UserSession) {
+        // TODO: implement profile loading API call
+    }
+}
